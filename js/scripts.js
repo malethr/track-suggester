5e14ee8db0d2d7b49b9a2d3ee9dd25228cfa5938
Maria Thomas <malethr@gmail.com>
2017-07-07 15:28:04 -0700

    Add branching statements.

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,7 @@ $(document).ready(function(){
     var answer3=$("input:radio[name=survey3]:checked").val();
     var answer4=$("input:radio[name=survey4]:checked").val();
     var answer5=$("input:radio[name=survey5]:checked").val();
+    var name=$("input#name").val();
 
     var ans1 = parseInt(answer1);
     var ans2 = parseInt(answer2);
@@ -17,13 +18,36 @@ $(document).ready(function(){
     var ans4 = parseInt(answer4);
     var ans5 = parseInt(answer5);
 
-    var totalanswer = add(ans1, ans2, ans3, ans4, ans5);
+    $("#username").text(name);
 
-    
+    var totalsum = add(ans1, ans2, ans3, ans4, ans5);
 
-    $("#intro").hide();
-    $("#formOne").hide();
+    $(".result").fadeIn();
 
+    if (totalsum === 5) {
+      $("#csstrack").show();
+      $("#javatrack").show();
+      $("#phptrack").hide();
+      $("#rubytrack").hide();
+      $("#csharptrack").show();
+    }
+    else if (totalsum <= 10) {
+      $("#csstrack").show();
+      $("#javatrack").show();
+      $("#phptrack").hide();
+      $("#rubytrack").show();
+      $("#csharptrack").hide();
+    }
+    else {
+      $("#csstrack").show();
+      $("#javatrack").hide();
+      $("#phptrack").show();
+      $("#rubytrack").show();
+      $("#csharptrack").hide();
+    }
+
+    $("#intro").fadeOut();
+    $("#formOne").fadeOut();
 
     event.preventDefault();
   });
