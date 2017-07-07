00dc5fbbf7a0b88aaae7d238893aef0650418aa8
Maria Thomas <malethr@gmail.com>
2017-07-07 13:49:00 -0700

    Add initial jquery file, image, modify html and add css styles.

--- img/Track Suggester_files/scripts.js
+++ img/Track Suggester_files/scripts.js
@@ -1,8 +1,3 @@
-var add = function(ans1, ans2, ans3, ans4, ans5) {
-  return ans1 + ans2 + ans3 + ans4 + ans5;
-};
-
-
 $(document).ready(function(){
   $("#formOne").submit(function(event){
     var answer1=$("input:radio[name=survey1]:checked").val();
@@ -17,12 +12,10 @@ $(document).ready(function(){
     var ans4 = parseInt(answer4);
     var ans5 = parseInt(answer5);
 
-    var totalanswer = add(ans1, ans2, ans3, ans4, ans5);
 
-    
 
     $("#intro").hide();
-    $("#formOne").hide();
+    $("#forOne").hide();
 
 
     event.preventDefault();
